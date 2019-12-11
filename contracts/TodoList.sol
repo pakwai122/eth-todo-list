54ff8ad1f01dde19d96433e58f0b73e09a0cb661
PW CHAN <pakwai122@protonmail.com>
2019-12-12 02:51:25 +0800

    4. tasks toggle

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -17,6 +17,10 @@ contract TodoList {
         string content,
         bool completed
     );
+    event TaskCompleted(
+        uint id,
+        bool completed
+    );
 
     constructor() public{
         createTask("PW");
@@ -27,4 +31,11 @@ contract TodoList {
         tasks[taskCount] = Task(taskCount, _content, false);
         emit TaskCreated(taskCount, _content, false);
     }
+function toggleCompleted(uint _id) public{
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+}
+
 }
